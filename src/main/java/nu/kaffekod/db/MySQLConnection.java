a804edc860e8db35d1d9540448f4e432614f5810
Monica Björk <kaffekod@protonmail.com>
2025-05-22 15:38:30 +0200

    mysql connection with login to localhost

--- src/main/java/nu/kaffekod/db/MySQLConnection.java
+++ src/main/java/nu/kaffekod/db/MySQLConnection.java
@@ -4,17 +4,19 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class MySQLConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/todoit";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/todoit?useSSL=false&serverTimezone=UTC";
 
     public static Connection getConnection() throws SQLException {
-        //Class.forName("com.mysql.jdbc.Driver");
         System.out.println("Log in to mysql");
+        /* */
         Scanner scanner = new Scanner(System.in);
         System.out.print("DB username: ");
         String dbUser = scanner.nextLine();
         System.out.print("DB password: ");
         String dbPassword = scanner.nextLine();
+        /* */
 
+        //Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, dbUser, dbPassword);
     }
 
